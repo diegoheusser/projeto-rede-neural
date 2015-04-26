20c0d9bce55c87119f8f68881d3aea731b132a5f
diegoheusser <diego.heusser@outlook.com>
2015-04-25 23:39:35 -0300

    Feito alterações na tela principal
    Feito a parte de desenho com Canvas e o evento do botão limpar

--- aplicacao/AppRedeNeural/src/appredeneural/Principal.java
+++ aplicacao/AppRedeNeural/src/appredeneural/Principal.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package appredeneural;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,6 +19,7 @@ public class Principal extends Application {
         Scene scene = new Scene(root, 600, 450);
         primaryStage.setTitle("Trabalho de Rede Neural");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
--- aplicacao/AppRedeNeural/src/appredeneural/controle/TelaPrincipal.java
+++ aplicacao/AppRedeNeural/src/appredeneural/controle/TelaPrincipal.java
@@ -1,13 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package appredeneural.controle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,12 +18,57 @@ import javafx.fxml.Initializable;
  */
 public class TelaPrincipal implements Initializable {
 
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Button btIdentificar;
+
+    @FXML
+    private Button btLimpar;
+
+    private GraphicsContext gc;
+
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        gc = canvas.getGraphicsContext2D();
+        iniciarEventos();
+    }
+
+    /**
+     * Método que inicia os eventos dos componentes visuais
+     */
+    private void iniciarEventos() {
+        
+        //Adicionando o evento para o canvas quando o mouse for pressionado
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
+            gc.beginPath();
+            gc.moveTo(event.getX(), event.getY());
+            gc.stroke();
+        });
+
+        //Adicionando o evento para o canvas quando o mouse for arrastado
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, (MouseEvent event) -> {
+            gc.lineTo(event.getX(), event.getY());
+            gc.stroke();
+        });
+
+        //Adicionando a ação do botão btLimpar
+        btLimpar.setOnAction((ActionEvent event) -> {
+            //Limpa a região de desenho
+            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        });
+
+        //Adicionando a ação do botão btIdentificar
+        btIdentificar.setOnAction((ActionEvent event) -> {
+            //Implementar
+        });
+    }
+
 }
