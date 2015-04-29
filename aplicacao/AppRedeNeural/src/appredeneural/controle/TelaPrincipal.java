086b2bbf32b2dcc12c26f3eb217aa9f246d80275
diegoheusser <diego.heusser@outlook.com>
2015-04-29 08:26:24 -0300

    Feito o evento de salvar o desenho em uma imagem png

--- aplicacao/AppRedeNeural/src/appredeneural/controle/TelaPrincipal.java
+++ aplicacao/AppRedeNeural/src/appredeneural/controle/TelaPrincipal.java
@@ -1,15 +1,23 @@
 package appredeneural.controle;
 
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 /**
  * FXML Controller class
@@ -45,7 +53,7 @@ public class TelaPrincipal implements Initializable {
      * Método que inicia os eventos dos componentes visuais
      */
     private void iniciarEventos() {
-        
+
         //Adicionando o evento para o canvas quando o mouse for pressionado
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
             gc.beginPath();
@@ -67,8 +75,33 @@ public class TelaPrincipal implements Initializable {
 
         //Adicionando a ação do botão btIdentificar
         btIdentificar.setOnAction((ActionEvent event) -> {
-            //Implementar
+            salvar();
         });
     }
 
+    private void salvar() {
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter
+                = new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(null);
+
+        if (file != null) {
+            try {
+                int width = (int)canvas.getWidth();
+                int height = (int) canvas.getHeight();
+                WritableImage writableImage = new WritableImage(width,height);
+                canvas.snapshot(null, writableImage);
+                RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+                ImageIO.write(renderedImage, "png", file);
+            } catch (IOException ex) {
+            }
+        }
+    }
 }
+
+
